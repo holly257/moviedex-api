97353f169d182ae22b0b56306b75f9c209580f93
Holly Rogers <hollyrogers@Hollys-Air.lan>
2020-03-26 20:38:20 -0400

    added filtering functions

--- server.js
+++ server.js
@@ -2,9 +2,42 @@ const express = require('express')
 const morgan = require('morgan')
 const helmet = require('helmet')
 const cors = require('cors')
-const MOIVES = require('./MOVIES.json')
+const MOVIES = require('./MOVIES.json')
 
 const app = express()
 app.use(morgan('dev'))
 app.use(helmet())
 app.use(cors())
+
+
+
+app.get('/movie', (req, res) => {
+    let response = MOVIES
+    const genre = req.query.genre
+    const country = req.query.country
+    const avg_vote = req.query.avg_vote
+
+    if(genre) {
+        response = response.filter(response => 
+            response.genre.toLowerCase().includes(genre.toLowerCase()))
+    }
+
+    if(country) {
+        response = response.filter(response => 
+            response.country.toLowerCase().includes(country.toLowerCase()))
+    }
+
+    if(avg_vote) {
+        response = response.filter(response => 
+            response.avg_vote >= avg_vote)
+    }
+
+
+    res.json(response)
+})
+
+const PORT = 8000
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`)
+})
